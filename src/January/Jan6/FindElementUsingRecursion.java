4c30d0cf7b08c6e656d1d307cf12fb7ee2d9485f
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2024-01-06 18:08:29 +0530

    FindElementUsingRecursion

---
+++ src/January/Jan6/FindElementUsingRecursion.java
@@ -0,0 +1,18 @@
+package January.Jan6;
+
+public class FindElementUsingRecursion {
+    public static void main(String[] args) {
+        int[] arr = {1,2,5,2,6,2,6};
+        int k = 10;
+        System.out.println(find(arr, k, 0));
+    }
+    public static int find(int[] arr, int k, int index){
+        if(arr[index] == k){
+            return index;
+        }
+        else if(index == arr.length-1){
+            return -1;
+        }
+        return find(arr, k, index+1);
+    }
+}
