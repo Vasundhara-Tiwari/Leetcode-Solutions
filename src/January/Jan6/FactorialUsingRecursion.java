6c5f5c18bf80780691f746d05918c529f48e73eb
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2024-01-06 17:02:18 +0530

    Factorial using recursion

--- src/January/Jan6/FactorialUsingRecursion.java
+++ src/January/Jan6/FactorialUsingRecursion.java
@@ -2,12 +2,12 @@ package January.Jan6;
 
 public class FactorialUsingRecursion {
     public static void main(String[] args) {
-        System.out.println(factorial(5));
+        System.out.println(factorial(10));
     }
     public static int factorial(int n){
         int fact = 1;
         if(n == 1 || n == 0){
-            return 1;
+            return fact;
         }
         fact = factorial(n-1) * n;
         return fact;
