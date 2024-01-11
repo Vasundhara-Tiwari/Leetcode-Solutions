772af518a3974d359403b37d6829bbfbce77dca5
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2024-01-11 22:38:38 +0530

    https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/

--- src/January/Jan11/FindTheIndexOfTheFirstOccurrenceInAString.java
+++ src/January/Jan11/FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -1,2 +1,20 @@
-package January.Jan11;public class FindTheIndexOfTheFirstOccurrenceInAString {
+package January.Jan11;
+
+public class FindTheIndexOfTheFirstOccurrenceInAString {
+    public static void main(String[] args) {
+        String haystack = "sadbutsad", needle = "sad";
+        System.out.println(strStr(haystack, needle));
+    }
+    public static int strStr(String haystack, String needle) {
+        int left = 0, right = needle.length();
+        while(right <= haystack.length()) {
+            if (haystack.substring(left, right).equals(needle))
+                return left;
+            else {
+                left++;
+                right++;
+            }
+        }
+        return -1;
+    }
 }
---
+++ src/January/Jan12/MissingNumber.java
@@ -0,0 +1,7 @@
+package January.Jan12;
+
+public class MissingNumber {
+    public static void main(String[] args) {
+
+    }
+}
