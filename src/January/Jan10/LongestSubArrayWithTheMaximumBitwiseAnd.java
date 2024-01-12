0b06218cde251feaa515702207c08717643c1f1e
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2024-01-12 22:16:05 +0530

    https://leetcode.com/problems/longest-subarray-with-maximum-bitwise-and/description/

--- src/January/Jan10/LongestSubArrayWithTheMaximumBitwiseAnd.java
+++ src/January/Jan10/LongestSubArrayWithTheMaximumBitwiseAnd.java
@@ -1,2 +1,25 @@
-package January.Jan10;public class LongestSubArrayWithTheMaximumBitwiseAnd {
+package January.Jan10;
+
+public class LongestSubArrayWithTheMaximumBitwiseAnd {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,3,2,2};
+        System.out.println(longestSubarray(nums));
+    }
+    public static int longestSubarray(int[] nums) {
+        int result = 0;
+        int length = 0;
+        int max = 0;
+        for (int ele : nums) {
+            max = Math.max(max, ele);
+        }
+        for (int ele : nums) {
+            if (ele == max) {
+                result = Math.max(result, ++length);
+            }
+            else {
+                length = 0;
+            }
+        }
+        return result;
+    }
 }
