92d0eb2fa1fd665a92af841b28d0c5160f2740bf
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2024-01-12 22:41:25 +0530

    https://leetcode.com/problems/missing-number/description/

--- src/January/Jan12/MissingNumber.java
+++ src/January/Jan12/MissingNumber.java
@@ -2,6 +2,17 @@ package January.Jan12;
 
 public class MissingNumber {
     public static void main(String[] args) {
-
+        int[] nums = {3,0,1};
+        System.out.println(missingNumber(nums));
+    }
+    public static int missingNumber(int[] nums) {
+        int n = nums.length;
+        long actualSum = (n*(n+1))/2;
+        long sum = 0;
+        for(int element: nums)
+        {
+            sum += element;
+        }
+        return (int)(actualSum-sum);
     }
 }
