1828d06832312bef5f1d80192b4e500a6e88b03c
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2024-01-14 16:43:29 +0530

    https://leetcode.com/problems/lexicographical-numbers/description/

--- src/January/Jan14/LexicalOrder.java
+++ src/January/Jan14/LexicalOrder.java
@@ -1,2 +1,21 @@
-package January.Jan14;public class LexicalOrder {
+package January.Jan14;
+
+public class LexicalOrder {
+    public static void main(String[] args) {
+        int n = 1000;
+        printLexicalOrder(n, 0);
+    }
+    public static void printLexicalOrder(int n, int current){
+        if(current > n){
+            return;
+        }
+        System.out.println(current);
+        int i = 0;
+        if(current == 0){
+            i = 1;
+        }
+        for(; i <= 9; i++){
+            printLexicalOrder(n, current*10 + i);
+        }
+    }
 }
