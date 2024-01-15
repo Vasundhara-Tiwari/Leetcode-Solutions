c968702b40cf6fbe96a8d452a082f875e75ecc6d
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2024-01-15 23:38:30 +0530

    https://leetcode.com/problems/longest-palindromic-substring/description/

---
+++ src/January/Jan15/LongestPalindromicSubstring.java
@@ -0,0 +1,42 @@
+package January.Jan15;
+
+import java.util.ArrayList;
+
+public class LongestPalindromicSubstring {
+    public static void main(String[] args) {
+
+    }
+
+    public String longestPalindrome(String s) {
+        ArrayList<String> al = new ArrayList<>();
+        for(int i = 0; i < s.length();  i++){
+            for(int j = i+1; j <= s.length(); j++){
+                String str = s.substring(i, j);
+                if(isPalindrome(str)){
+                    al.add(str);
+                }
+            }
+        }
+        String result = al.get(0);
+        for(int i = 1; i < al.size(); i++){
+            if(al.get(i).length() > result.length()){
+                result = al.get(i);
+            }
+        }
+        return result;
+    }
+    static boolean isPalindrome(String str){
+        int start = 0;
+        int end = str.length() - 1;
+        while(start <= end){
+            if(str.charAt(start) != str.charAt(end)){
+                return false;
+            }
+            else{
+                start++;
+                end--;
+            }
+        }
+        return true;
+    }
+}
