f9992323913e237fd581cafd9925aaeb81523024
Vasundhara-Tiwari <vasundhara.tiwari.321@gmail.com>
2024-07-01 23:08:31 +0530

    StringPermutation

--- src/June/StringPermutation.java
+++ src/June/StringPermutation.java
@@ -2,8 +2,10 @@ package June;
 
 public class StringPermutation {
     public static void main(String[] args) {
-        String str = "abc";
+        String str = "abca";
         printAllPermutations(str, "");
+        System.out.println("=================================");
+        printAllUniquePermutations(str, "");
     }
 
     private static void printAllPermutations(String str, String ans) {
@@ -16,6 +18,28 @@ public class StringPermutation {
             String s1 = str.substring(0, i);
             String s2 = str.substring(i+1);
             printAllPermutations(s1+s2, ans+ch);
+        }
+    }
+
+    private static void printAllUniquePermutations(String str, String ans) {
+        if(str.isEmpty()){
+            System.out.println(ans);
+            return;
+        }
+        for(int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+            boolean val = true;
+            for(int j = i +1; j < str.length(); j++){
+                if(str.charAt(j) == ch){
+                    val = false;
+                    break;
+                }
+            }
+            if(val) {
+                String s1 = str.substring(0, i);
+                String s2 = str.substring(i+1);
+                printAllUniquePermutations(s1+s2, ans+ch);
+            }
         }
     }
 }
