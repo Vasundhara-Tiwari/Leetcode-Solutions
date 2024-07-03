662eaeff3bac9ff47bb8534a1cba1c972d562d2c
Vasundhara-Tiwari <vasundhara.tiwari.321@gmail.com>
2024-07-03 10:05:16 +0530

    QueenPermutation - Backtracking

---
+++ src/June/QueenPermutation.java
@@ -0,0 +1,23 @@
+package June;
+
+public class QueenPermutation {
+    public static void main(String[] args) {
+        int n = 4;
+        boolean[] board = new boolean[n];
+        int q = 2;
+        queenPermutation(board, q, "", 0);
+    }
+    public static void queenPermutation(boolean[] board, int tq, String ans, int qpsf){
+        if(qpsf == tq){
+            System.out.println(ans);
+            return;
+        }
+        for(int i = 0; i < board.length; i++){
+            if(board[i] == false){
+                board[i] = true;
+                queenPermutation(board, tq, ans+" b"+i+"q"+qpsf, qpsf+1);
+                board[i] = false;
+            }
+        }
+    }
+}
