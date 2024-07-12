da24047d5eef62954d657abffae140395da3d5c2
Vasundhara-Tiwari <vasundhara.tiwari.321@gmail.com>
2024-07-12 19:04:03 +0530

    ReverseQueue

---
+++ src/July/ReverseQueue.java
@@ -0,0 +1,28 @@
+package July;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ReverseQueue {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(10);
+        q.add(20);
+        q.add(30);
+        q.add(40);
+        q.add(50);
+        System.out.println(q);
+        reverseQueue(q);
+        System.out.println(q);
+    }
+
+    private static void reverseQueue(Queue<Integer> q) {
+        if(q.isEmpty()){
+            return;
+        }
+        int item = q.peek();
+        q.remove();
+        reverseQueue(q);
+        q.add(item);
+    }
+}
