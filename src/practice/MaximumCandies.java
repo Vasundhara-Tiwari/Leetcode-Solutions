294873e8855e960dffc96d1a3c28d7a40d2ce2d5
Vasundhara Tiwari <developer.vasundharatiwari@gmail.com>
2025-03-14 23:02:13 +0530

    2226. Maximum Candies Allocated to K Children

---
+++ src/practice/MaximumCandies.java
@@ -0,0 +1,31 @@
+public class MaximumCandies{
+    public int maximumCandies(int[] candies, long k) {
+        long sum = 0;
+        int max = 0;
+        for (int i : candies) {
+            sum += i;
+            max = Math.max(max, i);
+        }
+        
+        if (sum < k) return 0;
+        if (sum == k) return 1;
+        
+        int l = 1, r = max;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (isPossible(candies, k, mid)) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return r;
+    }
+    public boolean isPossible(int[] candies, long k, int candy) {
+        long child = 0;
+        for (int i : candies) {
+            child += i / candy;
+        }
+        return (child >= k);
+    }
+}
