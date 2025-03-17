898384714eacb6c47b101ae91b3530435142d59a
Vasundhara Tiwari <developer.vasundharatiwari@gmail.com>
2025-03-17 19:02:30 +0530

    2206. Divide Array Into Equal Pairs

---
+++ src/practice/DivideArrayIntoEqualPairs.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class DivideArrayIntoEqualPairs{
+    public boolean divideArray(int[] nums) {
+        Map<Integer, Integer> frequency = new HashMap<>();
+        for(int ele: nums){
+            frequency.put(ele, frequency.getOrDefault(ele, 0) + 1);
+        }
+        for(int count: frequency.values()){
+            if(count % 2 != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
