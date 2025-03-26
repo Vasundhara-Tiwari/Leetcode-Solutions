428c6fe53d73cf50e5037951a2ebf17eee1f023c
Vasundhara Tiwari <developer.vasundharatiwari@gmail.com>
2025-03-26 17:44:50 +0530

    2033. Minimum Operations to Make a Uni-Value Grid

---
+++ src/practice/MinimumOperationsToMake UniValueGrid.java
@@ -0,0 +1,26 @@
+package practice;
+
+public class MinimumOperationsToMake {
+    public int minOperations(int[][] grid, int x) {
+        List<Integer> values = new ArrayList<>();
+        for (int[] row : grid) {
+            for (int val : row) {
+                values.add(val);
+            }
+        }
+
+        for (int val : values) {
+            if (Math.abs(val - values.get(0)) % x != 0) {
+                return -1;
+            }
+        }
+
+        Collections.sort(values);
+        int median = values.get(values.size() / 2);
+        int op = 0;
+        for(int i = 0; i < values.size(); i++){
+            op += Math.abs(values.get(i) - median) / x;
+        }
+        return op;
+    }
+}
